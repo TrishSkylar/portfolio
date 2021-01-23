f9bc54d96d424079fbc100d2c34b222911bcb332
Derry Sucari <dsucaria@ulasalle.edu.pe>
2021-01-23 08:00:17 -0500

    adding Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { APP_NAME } from '../../utils/constants';
+import BannerImage from '../../assets/webp/banner.webp';
 import './Home.css';
 
 export default props => {
   const { data } = props;
-  console.log(data);
   return (
-    <main className="home">
-      <section className="left-home">
+    <main className="home flex-container">
+      <section className="left-home flex-container">
+        <h1>{ data.title }</h1>
+        <p>{ data.description }</p>
       </section>
-      <section className="right-home">
+      <section className="right-home flex-container">
+        <img
+          alt={ `${APP_NAME} banner` }
+          src={ BannerImage }
+          width="280"
+          height="280"
+        />
       </section>
     </main>
   );
