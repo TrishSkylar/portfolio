1d51093a8a41d1c4e4777685349271819d9d8972
Derry Sucari <dsucaria@ulasalle.edu.pe>
2021-01-22 23:08:31 -0500

    configuration webpack.prod.js

---
+++ webpack.prod.js
@@ -0,0 +1,47 @@
+const { merge } = require('webpack-merge');
+const webpackCommon = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { DefinePlugin } = require('webpack');
+
+module.exports = merge(webpackCommon, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'main',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env': {
+        APP_NAME: JSON.stringify(process.env.APP_NAME),
+        API_HOST: JSON.stringify(process.env.API_HOST)
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].min.css',
+    })
+  ]
+});
